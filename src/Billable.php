c0cb8d2a751b3721afc1a55ec31b595c90a2e0a3
Angus Moore <angusmoore73@gmail.com>
2019-05-10 11:58:48 +1000

    Fixed incorrect function call

--- src/Billable.php
+++ src/Billable.php
@@ -712,7 +712,7 @@ trait Billable
             return $subscription->addItem($plan, $prorate, $quantity);
         }
 
-        return $this->newMultisubscription($subscription)->addPlan($plan, $prorate, $quantity)->create();
+        return $this->newMultisubscription($subscription)->addPlan($plan, $quantity)->create();
     }
 
     /**
